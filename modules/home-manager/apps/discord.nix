c66c3f14c570c08ac5971c576392f66fe06e2e4f
l-eg116 <legcom6@gmail.com>
2024-04-23 15:36:28 +0200

    Made vesktop look like discord

--- modules/home-manager/apps/discord.nix
+++ modules/home-manager/apps/discord.nix
@@ -3,4 +3,13 @@
   home.packages = with pkgs; [
     vesktop
   ];
+
+  xdg.desktopEntries."vesktop" = {
+    name = "Discord";
+    genericName = "Internet Messenger";
+    exec = "vesktop";
+    terminal = false;
+    icon = "discord";
+    categories = [ "Network" "Utility" ];
+  };
 }
