cf37ee45056919378e1a2780ea91546262d5e4a8
l-eg116 <legcom6@gmail.com>
2024-11-19 11:03:23 +0100

    Made ArcWTF toggelable

--- modules/home-manager/apps/arcwtf.nix
+++ modules/home-manager/apps/arcwtf.nix
@@ -1,95 +1,101 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 
 {
-  programs.firefox = {
-    enable = true;
-    package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-      extraPolicies = {
-        DisableTelemetry = true;
-        DisableFirefoxStudies = true;
-        EnableTrackingProtection = {
-          Value= true;
-          Locked = true;
-          Cryptomining = true;
-          Fingerprinting = true;
+  options = {
+    desktop.arc-wtf.enable = lib.mkEnableOption "Enables Arc Wtf (heavily moded Firefox)";
+  };
+
+  config = lib.mkIf config.desktop.arc-wtf.enable {
+    programs.firefox = {
+      enable = true;
+      package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
+        extraPolicies = {
+          DisableTelemetry = true;
+          DisableFirefoxStudies = true;
+          EnableTrackingProtection = {
+            Value = true;
+            Locked = true;
+            Cryptomining = true;
+            Fingerprinting = true;
+          };
+          DisablePocket = true;
+          DisableFirefoxAccounts = true;
+          DisableAccounts = true;
+          DisableFirefoxScreenshots = true;
+          OverrideFirstRunPage = "";
+          OverridePostUpdatePage = "";
+          DontCheckDefaultBrowser = true;
+
+          ExtensionSettings = {
+            # ArcWTF dependencies
+            # Sidebery
+            "{3c078156-979c-498b-8990-85f7987dd929}" = {
+                install_url = "https://addons.mozilla.org/firefox/downloads/latest/sidebery/latest.xpi";
+                installation_mode = "force_installed";
+            };
+            # Userchrome toggle
+            "userchrome-toggle@joolee.nl" = {
+                install_url = "https://addons.mozilla.org/firefox/downloads/latest/userchrome-toggle/latest.xpi";
+                installation_mode = "force_installed";
+            };
+            # FireFox Colors
+            "FirefoxColor@mozilla.com" = {
+                install_url = "https://addons.mozilla.org/firefox/downloads/latest/firefox-color/latest.xpi";
+                installation_mode = "force_installed";
+            };
+
+            # uBlock Origin
+            "uBlock0@raymondhill.net" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+              installation_mode = "force_installed";
+            };
+            # Dark Reader
+            "addon@darkreader.org" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+              installation_mode = "force_installed";
+            };
+            # Bitwarden
+            "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+              installation_mode = "force_installed";
+            };
+          };
         };
-        DisablePocket = true;
-        DisableFirefoxAccounts = true;
-        DisableAccounts = true;
-        DisableFirefoxScreenshots = true;
-        OverrideFirstRunPage = "";
-        OverridePostUpdatePage = "";
-        DontCheckDefaultBrowser = true;
+      };
 
-        ExtensionSettings = {
-          # ArcWTF dependencies
-          # Sidebery
-          "{3c078156-979c-498b-8990-85f7987dd929}" = {
-              install_url = "https://addons.mozilla.org/firefox/downloads/latest/sidebery/latest.xpi";
-              installation_mode = "force_installed";
-          };
-          # Userchrome toggle
-          "userchrome-toggle@joolee.nl" = {
-              install_url = "https://addons.mozilla.org/firefox/downloads/latest/userchrome-toggle/latest.xpi";
-              installation_mode = "force_installed";
-          };
-          # FireFox Colors
-          "FirefoxColor@mozilla.com" = {
-              install_url = "https://addons.mozilla.org/firefox/downloads/latest/firefox-color/latest.xpi";
-              installation_mode = "force_installed";
-          };
+      profiles.default = {
+        id = 0;
+        name = "default";
+        isDefault = true;
 
-          # uBlock Origin
-          "uBlock0@raymondhill.net" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # Dark Reader
-          "addon@darkreader.org" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # Bitwarden
-          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-            installation_mode = "force_installed";
-          };
+        settings = {
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "svg.context-properties.content.enabled" = true;
+          "uc.tweak.popup-search" = true;
+          "uc.tweak.longer-sidebar" = true;
+          "browser.startup.page" = 3;
+          "browser.ctrlTab.sortByRecentlyUsed" = true;
+
+          "widget.use-xdg-desktop-portal.file-picker" = 1;
+          "widget.use-xdg-desktop-portal.mime-handler" = 1;
+          "browser.download.useDownloadDir" = false;
         };
       };
     };
 
-    profiles.default = {
-      id = 0;
-      name = "default";
-      isDefault = true;
-
-      settings = {
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-        "svg.context-properties.content.enabled" = true;
-        "uc.tweak.popup-search" = true;
-        "uc.tweak.longer-sidebar" = true;
-        "browser.startup.page" = 3;
-        "browser.ctrlTab.sortByRecentlyUsed" = true;
-
-        "widget.use-xdg-desktop-portal.file-picker" = 1;
-        "widget.use-xdg-desktop-portal.mime-handler" = 1;
-        "browser.download.useDownloadDir" = false;
-      };
+    home.file."firefox_chrome" = {
+      source = ./arcwtf;
+      target = ".mozilla/firefox/default/chrome";
+      recursive = true;
     };
-  };
 
-  home.file."firefox_chrome" = {
-    source = ./arcwtf;
-    target = ".mozilla/firefox/default/chrome";
-    recursive = true;
-  };
-
-  xdg.desktopEntries."firefox" = {
-    name = "Arc WTF";
-    genericName = "Web Browser";
-    exec = "firefox %u";
-    terminal = false;
-    icon = ./arcwtf/Arc.ico;
-    categories = [ "Application" "Network" "WebBrowser" ];
+    xdg.desktopEntries."firefox" = {
+      name = "Arc WTF";
+      genericName = "Web Browser";
+      exec = "firefox %u";
+      terminal = false;
+      icon = ./arcwtf/Arc.ico;
+      categories = [ "Application" "Network" "WebBrowser" ];
+    };
   };
 }
